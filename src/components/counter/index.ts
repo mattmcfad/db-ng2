02cff5f1c41f9625448e9885384f6b7e4a7eadef
mattmcfad <mmcfadyen91@gmail.com>
2016-07-05 10:25:18 -0400

    'Created db-ng2 from angular2-redux-starter'

---
+++ src/components/counter/index.ts
@@ -0,0 +1,37 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { RioButton } from '../button';
+
+@Component({
+  selector: 'rio-counter',
+  template: `
+    <div class="flex">
+      <rio-button
+        className="bg-black col-2"
+        (onClick)="decrement.emit()">
+        -
+      </rio-button>
+
+      <div class="flex-auto flex-center center h1">
+        {{ counter }}
+      </div>
+
+      <rio-button className="col-2"
+        (onClick)="increment.emit()">
+        +
+      </rio-button>
+    </div>
+  `,
+  directives: [RioButton],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RioCounter {
+  @Input() counter: number;
+  @Output() increment = new EventEmitter<void>();
+  @Output() decrement = new EventEmitter<void>();
+};
